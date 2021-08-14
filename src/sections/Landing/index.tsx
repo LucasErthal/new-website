5f357472c0cfd00717b1b7afaf9b169ebc187d6a
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2021-08-13 23:08:03 -0300

    Update index.tsx

--- src/sections/Landing/index.tsx
+++ src/sections/Landing/index.tsx
@@ -18,8 +18,15 @@ function Landing() {
         >
           Lucas de Oliveira Mattos Erthal
         </SubTitle>
-        
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+        Olá! Bem-vindo(a) ao meu site de portfólio.
+        Sou um profissional de tecnologia, com experiência na área de programação, me especializando em
+        desenvolvimento de aplicações mobile e web. Possuo experiência nas seguintes atividades:
+
+        Desenvolvimento de aplicações mobile com React-Native e web com ReactJS utilizando Javascript e Typescript,
+        integração com Firebase e Cloud Functions, NodeJS, Knex, TypeORM.
+        Criação de documentação, levantamento de regras de negócio, requisitos de sistema e diagrama UML.
+        Utilizo tanto SQL quanto NoSQL.
       </Styled.ProfileContainer>
 
       <Styled.LandingImage src={LandingImage} alt="" />
